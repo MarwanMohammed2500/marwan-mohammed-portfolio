9f6d7d2e21884f2c0587afdeb52f128b225748ca
MarwanMohammed2500 <marwanmohammed2500@gmail.com>
2025-07-17 17:54:08 +0300

    modified:   .firebase/hosting.ZGlzdC9zZXJ2ZXIvcHVibGlj.cache 	modified:   client/src/components/projects.tsx

--- client/src/components/projects.tsx
+++ client/src/components/projects.tsx
@@ -11,7 +11,7 @@ export default function Projects() {
       description: "A practical MLOps-focused project demonstrating the full lifecycle of a deep learning model using FashionMNIST — from modular model design to deployment with FastAPI, Docker, and Kubernetes. Emphasis is on production-readiness, reproducibility, and clean backend workflows.",
       image: "https://www.databricks.com/sites/default/files/inline-images/mlops-cycle.png?v=1673970772",
       tags: ["PyTorch", "FastAPI", "Docker", "Kubernetes", "TensorBoard", "TorchScript", "Matplotlib", "Modular ML Design"],
-      github: "https://github.com/MarwanMohammed2500/FashionMNIST_Modular_Deployed",
+      github: "https://github.com/MarwanMohammed2500/End-to-End-MLOps-Pipeline-Deep-Learning-Deployment",
       demo: null
     },
     {
